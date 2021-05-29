6b09999c6480ec512739a3d250a38c29482283f1
Manjeetdhimaan <manjeetdhimaan80@gmail.com>
2021-05-29 20:06:45 +0530

    1 file modified

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,7 +3,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 
 
 @Injectable()

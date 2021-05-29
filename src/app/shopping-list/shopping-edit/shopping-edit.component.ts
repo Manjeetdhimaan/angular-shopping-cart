ebfdd5e97b0ff43587909b32c72f7a9a962c66c7
Manjeetdhimaan <manjeetdhimaan80@gmail.com>
2021-05-29 20:11:57 +0530

    1 file modified

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/core';
 
 import { NgForm } from '@angular/forms';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
